e8d528ad9b1ccbff6088d8517fda16dea9ed5947
Damian Gryski <damian@gryski.com>
2011-09-13 23:03:19 +0200

    Make status and counter routines more closely match existing Java API.

--- reporter.go
+++ reporter.go
@@ -5,16 +5,19 @@ import (
 	"fmt"
 )
 
-func Reportln(a ...interface{}) {
+// Statusln updates the Hadoop job status.  The arguments are passed to fmt.Sprintln
+func Statusln(a ...interface{}) {
 	s := fmt.Sprintln(a...)
 	fmt.Fprintf(os.Stderr, "reporter:status:%s", s) // \n is in s
 }
 
-func Reportf(format string, a ...interface{}) {
+// Statusf updates the Hadoop job status.  The arguments are passed to fmt.Sprintf
+func Statusf(format string, a ...interface{}) {
 	s := fmt.Sprintf(format, a...) // we should check if s contains \n
 	fmt.Fprintf(os.Stderr, "reporter:status:%s\n", s)
 }
 
+// IncrCounter updates the given group/counter by 'amount'
 func IncrCounter(group, counter string, amount int) {
 	fmt.Fprintf(os.Stderr, "reporter:counter:%s,%s,%d\n", group, counter, amount)
 }
