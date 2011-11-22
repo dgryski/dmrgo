d4d2149396181c8439ce62f657bb03887ee4b69d
Damian Gryski <damian@gryski.com>
2011-11-23 00:48:28 +0100

    First draft of tab-separated protocol.
    The JSON one is too slow to be used in production.  In my tests, it's
    about 15 times slower than just scalling Sscan.  This a simpler
    text-based serialization format.  We write out lines of values,
    separated by tabs.  For a struct or an array, we just print out the
    elements/fields in order.  Otherwise, it pretty much ends up as the
    single-valued string representation of the primitive type
    (int/float/string).
    
    For the unmarshal side, I split on tabs and then fill in the values in
    order of the struct that was passed in.  Arrays aren't handled yet
    (it's late), but should be "trivial" to add.
    
    This almost certianly need some cleanup, but for now it appears to
    work as a proof-of-concept.

--- proto.go
+++ proto.go
@@ -6,19 +6,22 @@ package dmrgo
 
 import (
 	"encoding/json"
+	"fmt"
 	"reflect"
+	"strconv"
+	"strings"
 )
 
 // MRProtocol is a set of routines for marshaling and unmarshaling key/value pairs from the input stream.
 // Map Reduce jobs can define their own protocols.
 type MRProtocol interface {
 
-        // UnmarshalKV turns strings into their associated values.
-        // k should be a pointer to the destination value for the unmarshalled "key"
-        // vs should be a pointer to an array for the unmarshalled "values"
+	// UnmarshalKV turns strings into their associated values.
+	// k should be a pointer to the destination value for the unmarshalled "key"
+	// vs should be a pointer to an array for the unmarshalled "values"
 	UnmarshalKVs(key string, values []string, k interface{}, vs interface{})
 
-        // MarshalKV turns a key/value pair into a KeyValue 
+	// MarshalKV turns a key/value pair into a KeyValue 
 	MarshalKV(key interface{}, value interface{}) *KeyValue
 }
 
@@ -53,4 +56,106 @@ func (p *JSONProtocol) MarshalKV(key interface{}, value interface{}) *KeyValue {
 	k, _ := json.Marshal(key)
 	v, _ := json.Marshal(value)
 	return &KeyValue{string(k), string(v)}
+}
+
+type TSVProtocol struct {
+	// empty -- just a type
+}
+
+func (p *TSVProtocol) MarshalKV(key interface{}, value interface{}) *KeyValue {
+
+	kVal := reflect.ValueOf(key)
+	k := primitiveToString(kVal)
+
+	var vs []string
+
+	vType := reflect.TypeOf(value)
+	vVal := reflect.ValueOf(value)
+
+	if vType.Kind() == reflect.Struct {
+		for i := 0; i < vType.NumField(); i++ {
+			field := vVal.Field(i)
+			v := primitiveToString(field)
+			vs = append(vs, v)
+		}
+	} else if vType.Kind() == reflect.String {
+		vs = append(vs, vVal.String())
+	} else if vType.Kind() == reflect.Array || vType.Kind() == reflect.Slice {
+		for i := 0; i < vVal.Len(); i++ {
+			field := vVal.Index(i)
+			v := fmt.Sprint(field.Interface())
+			vs = append(vs, v)
+		}
+	}
+
+	vals := strings.Join(vs, "\t")
+
+	return &KeyValue{k, vals}
+}
+
+func (p *TSVProtocol) UnmarshalKVs(key string, values []string, k interface{}, vs interface{}) {
+
+	fmt.Sscan(key, &k)
+
+	vsPtrValue := reflect.ValueOf(vs)
+	vsType := reflect.TypeOf(vs).Elem()
+	vType := vsType.Elem()
+
+	v := reflect.New(vsType).Elem()
+
+	for _, s := range values {
+		vs := strings.Split(s, "\t")
+
+		if vType.Kind() == reflect.Struct {
+			e := reflect.New(vsType.Elem())
+			for i := 0; i < vType.NumField(); i++ {
+				_, err := fmt.Sscan(vs[i], e.Elem().Field(i).Addr().Interface())
+				if err != nil {
+					continue // skip
+				}
+			}
+			v = reflect.Append(v, e.Elem())
+		}
+	}
+
+	vsPtrValue.Elem().Set(v)
+}
+
+func primitiveToString(v reflect.Value) string {
+
+	switch v.Kind() {
+
+	case reflect.Bool:
+		return fmt.Sprint(v.Bool())
+	case reflect.Int:
+		fallthrough
+	case reflect.Int8:
+		fallthrough
+	case reflect.Int16:
+		fallthrough
+	case reflect.Int32:
+		fallthrough
+	case reflect.Int64:
+		return strconv.Itoa64(v.Int())
+
+	case reflect.Uint:
+		fallthrough
+	case reflect.Uint8:
+		fallthrough
+	case reflect.Uint16:
+		fallthrough
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Uint64:
+		return strconv.Uitoa64(v.Uint())
+
+	case reflect.Float32:
+		fallthrough
+	case reflect.Float64:
+		return strconv.Ftoa64(v.Float(), 'g', 5)
+	case reflect.String:
+		return v.String()
+	}
+
+	return "(unknown)"
 }
